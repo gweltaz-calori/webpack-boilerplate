24e5742643602679ae03abd383e32e4a98f97790
FRANCONY Romain <r.francony@gmail.com>
2017-08-19 11:13:21 +0200

    Add sass and scss support

--- build/config.js
+++ build/config.js
@@ -21,12 +21,18 @@ module.exports = {
                 loader: 'babel-loader'
             },
             {
-                test: /\.css$/,
+                test: /\.(s)?css$|\.sass$/,
                 use: ExtractTextPlugin.extract({
                   fallback: "style-loader",
                   use: [
                     {
                         loader : 'css-loader',
+                        options : {
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
                         options : {
                             minimize: true
                         }
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import './assets/css/style.css'
+import './assets/styles/style.css';
+import './assets/styles/compiled.scss';
 
 import {sayHello} from './autre'
-sayHello()
+sayHello();
 
