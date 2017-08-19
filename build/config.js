5aa61f6a267754be2f41342cc7e559768b0d55cf
FRANCONY Romain <r.francony@gmail.com>
2017-08-19 10:57:54 +0200

    Update gitignore, fix dev script

--- build/config.js
+++ build/config.js
@@ -1,10 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
 
-const DEV = process.env.NODE_ENV == "dev"
+const DEV = process.env.NODE_ENV == "dev";
 
 module.exports = {
     entry: './src/app.js',
